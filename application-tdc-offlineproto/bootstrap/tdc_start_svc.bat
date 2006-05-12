3bf590d0231b080805a55a7f61052f9d02d37886
ggennaro <>
2006-05-12 21:11:48 +0000

    temporary bootstrap

---
+++ application-tdc-offlineproto/bootstrap/tdc_start_svc.bat
@@ -0,0 +1,14 @@
+cls
+cd ..
+@set CURPATH=%cd%
+set JETTY_HOME=%CURPATH%\servletcontainer\jetty-5.1.11RC0
+@rem set CLASSPATH=C:\apps\Java\jdk1.5.0_04\lib\tools.jar
+set CLASSPATH=%CLASSPATH%;%JETTY_HOME%\lib\org.mortbay.jetty.jar
+set CLASSPATH=%CLASSPATH%;%JETTY_HOME%\lib\javax.servlet.jar
+set CLASSPATH=%CLASSPATH%;%JETTY_HOME%\ext\jasper-runtime.jar
+set CLASSPATH=%CLASSPATH%;%JETTY_HOME%\ext\jasper-compiler.jar
+set CLASSPATH=%CLASSPATH%;%JETTY_HOME%\ext\xercesImpl.jar
+set JETTY_HOME
+set CLASSPATH
+java -Djetty.home=%JETTY_HOME% -jar -Dtdc.webapp=%CURPATH%\webapp %JETTY_HOME%\start.jar %JETTY_HOME%\etc\tdc.xml
+exit
