a7b1eb2cc967987cec355e9fabadc85280ff3c43
ggennaro <>
2006-05-13 00:52:20 +0000

    *** empty log message ***

--- application-tdc-offlineproto/bootstrap/tdc.bat
+++ application-tdc-offlineproto/bootstrap/tdc.bat
@@ -1,4 +1,4 @@
-@set CURPATH=%cd%
-@start /MIN tdc_start_svc.bat 
-@"C:\Program Files\Internet Explorer\IEXPLORE.EXE" -k file://%CURPATH%/loading.html
-@start /MIN tdc_stop_svc.bat
+set CURPATH=%cd%
+start /MIN tdc_start_svc.bat 
+"C:\Program Files\Internet Explorer\IEXPLORE.EXE" file://%CURPATH%/loading.html
+start /MIN tdc_stop_svc.bat
--- application-tdc-offlineproto/bootstrap/tdc_stop_svc.bat
+++ application-tdc-offlineproto/bootstrap/tdc_stop_svc.bat
@@ -1,8 +1,7 @@
 cls
 cd ..
-@set CURPATH=%cd%
+set CURPATH=%cd%
 set JETTY_HOME=%CURPATH%\servletcontainer\jetty-5.1.11RC0
-@rem set CLASSPATH=C:\apps\Java\jdk1.5.0_04\lib\tools.jar
 set CLASSPATH=%CLASSPATH%;%JETTY_HOME%\lib\org.mortbay.jetty.jar
 set CLASSPATH=%CLASSPATH%;%JETTY_HOME%\lib\javax.servlet.jar
 set CLASSPATH=%CLASSPATH%;%JETTY_HOME%\ext\jasper-runtime.jar
@@ -10,5 +9,5 @@ set CLASSPATH=%CLASSPATH%;%JETTY_HOME%\ext\jasper-compiler.jar
 set CLASSPATH=%CLASSPATH%;%JETTY_HOME%\ext\xercesImpl.jar
 set JETTY_HOME
 set CLASSPATH
-java -Djetty.home=%JETTY_HOME% -jar -Dtdc.webapp=%CURPATH%\webapp %JETTY_HOME%\stop.jar %JETTY_HOME%\etc\tdc.xml
+java -Djetty.home="%JETTY_HOME%" -Dtdc.webapp="%CURPATH%\webapp" -jar "%JETTY_HOME%\stop.jar" "%JETTY_HOME%\etc\tdc.xml"
 exit
