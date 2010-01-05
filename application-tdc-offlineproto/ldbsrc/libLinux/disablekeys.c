2c8b54556fb1a8d222a8b4dd1b7499379d3fdcab
ayanb <>
2010-01-05 18:30:35 +0000

    Linux changes

--- application-tdc-offlineproto/ldbsrc/libLinux/disablekeys.c
+++ application-tdc-offlineproto/ldbsrc/libLinux/disablekeys.c
@@ -2,15 +2,13 @@
 
 int main()
 {
-system("xmodmap -e \"pointer = 1 9 8 7 6 5 4 3 2\"");
 system("sh createoriginal.sh");
-system("gcc -g -o testobj test.c");
-system("./testobj");
+system("gcc -g -o xmodmapobj  ModifyXmodmap.c");
+system ("chmod 777 xmodmapobj");
+system("./xmodmapobj");
 system("sh loadmodified.sh");
 system("sh print_screen_disable.sh");
 system("sh processcheck.sh");
-system("gcc -g -o processcheckobj processcheck.c");
-system("./processcheckobj");
 //system("find /usr/bin -type f -name \"gnome-panel-screenshot\" -exec chmod 600 {} \\;");
 return 0;
 }
--- application-tdc-offlineproto/ldbsrc/libLinux/enablekeys.c
+++ application-tdc-offlineproto/ldbsrc/libLinux/enablekeys.c
@@ -2,7 +2,6 @@
 
 int main()
 {
-system("xmodmap -e \"pointer = 1 2 3 4 5 6 7 8 9\"");
 system("sh loadoriginal.sh");
 //system("sh print_screen_enable.sh");
 //system("find /usr/bin -type f -name \"gnome-panel-screenshot\" -exec chmod 755 {} \\;");
