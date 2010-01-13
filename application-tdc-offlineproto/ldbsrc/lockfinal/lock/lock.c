50caf0f3d8db28c3d558acc21f186072d5593b28
ncohen <>
2010-01-13 00:26:20 +0000

    vista taskmgr fix

--- application-tdc-offlineproto/ldbsrc/lockfinal/lock/lock.c
+++ application-tdc-offlineproto/ldbsrc/lockfinal/lock/lock.c
@@ -382,7 +382,12 @@ int DLL_EXP_IMP WINAPI CTRLALTDEL_Enable_Disable(BOOL bEnableDisable)
 JNIEXPORT jboolean JNICALL Java_com_ctb_tdc_bootstrap_processwrapper_LockdownBrowserWrapper_Kill_1Task_1Mgr
   (JNIEnv *env, jclass obj)
 {
-	return CheckForTaskMgr();
+	while (!finished) { 
+		CheckForTaskMgr();
+		Sleep(1000);
+	}
+	return FALSE;
+	
 }
 /*****************************************************************
  * Check for TaskMgr.exe *
@@ -427,7 +432,6 @@ DLL_EXP_IMP BOOL WINAPI CheckForTaskMgr()
 						if ((0 == strcmp(szProcessName, "taskmgr.exe")) || (0 == strcmp(szProcessName, "Taskmgr.exe")))
 						{
 							system("taskkill /F /IM taskmgr.exe /T");
-							system("taskkill /F /IM Taskmgr.exe /T");
 
 							//WinExec("taskkill /IM taskmgr.exe",SW_HIDE);
 							return 1;
