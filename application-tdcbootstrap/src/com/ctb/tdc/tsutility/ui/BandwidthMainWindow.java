7c501f2548b841276a42614395ce003c55af0ec9
ncohen <>
2010-09-29 15:55:54 +0000

    reduce number of threads used by bandwidth simulator

--- application-tdcbootstrap/src/com/ctb/tdc/tsutility/ui/BandwidthMainWindow.java
+++ application-tdcbootstrap/src/com/ctb/tdc/tsutility/ui/BandwidthMainWindow.java
@@ -52,6 +52,8 @@ public class BandwidthMainWindow extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	
+	private static final int DIVISOR = 5;
+	
 	private static final Dimension WINDOW_DIMENSIONS = new Dimension(650,550);
 	private static final Color WINDOW_BACKGROUND_COLOR = SystemColor.control;
 	private static final Font WINDOW_FONT_PLAIN = new Font("Arial", Font.PLAIN, 12);  
@@ -808,7 +810,7 @@ public class BandwidthMainWindow extends JFrame implements ActionListener {
 
 		System.out.println("endSimulation ( " + numberMachines + " )   ---   time spent = " + numberSeconds + " seconds   ---   totalBytesRead = " + byteReadStr + "    ---   bandwidth = " + bandwidthStr + " Kbps");
 		
-    	String text = " " + numberMachines + "\t\t";		
+    	String text = " " + (numberMachines * DIVISOR) + "\t\t";		
     	insertReportString(text, "regular");
     	String description = "";
     	
@@ -838,7 +840,7 @@ public class BandwidthMainWindow extends JFrame implements ActionListener {
 		
     	insertReportString("\n", "regular");
     		
-    	this.outputText += numberMachines  + "\t\t" + byteReadStr + "\t" + bandwidthStr + "\t\t" + description;	
+    	this.outputText += (numberMachines * DIVISOR)  + "\t\t" + byteReadStr + "\t" + bandwidthStr + "\t\t" + description;	
     	this.outputText += System.getProperty("line.separator");
     	
     	this.repaint();
@@ -1020,17 +1022,20 @@ public class BandwidthMainWindow extends JFrame implements ActionListener {
 		
 		ArrayList list = new ArrayList();
 
-		int numInterval = maxMachines / 4;
+		int numInterval = (maxMachines / 4) / DIVISOR;
+		if(numInterval < 1) numInterval = 1;
 			
 		int num = numInterval;
-		while (num <= maxMachines) {
+		while (num <= (maxMachines/DIVISOR)) {
 			list.add(new Integer(num));		
 			num += numInterval;
 		}			
 		
 		Integer value = (Integer)list.get(list.size()-1);		
-		if (value.intValue() != maxMachines) {
-			list.add(new Integer(maxMachines));		
+		if (value.intValue() != (maxMachines/DIVISOR)) {
+			num = maxMachines / DIVISOR;
+			if(num < 1) num = 1;
+			list.add(new Integer(num));		
 		}
 
 		return list;
