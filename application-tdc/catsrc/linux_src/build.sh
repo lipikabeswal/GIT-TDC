cf30f4366d6e8231decc5d926055ac11d7402ba2
nirmalab <>
2012-06-29 18:52:54 +0000

    linux build script

--- application-tdc/catsrc/linux_src/build.sh
+++ application-tdc/catsrc/linux_src/build.sh
@@ -1,3 +1,3 @@
 rm -rf *.o
 rm -rf *.so
-gcc -o libCATABE.so -shared -fPIC -I /usr/java/jdk1.6.0_21/include -I /usr/java/jdk1.6.0_21/include/linux array.c item.c irt.c sort_index.c utilityFun.c genRandomDis.c getPar.c cat.c -I.
+gcc -o libCATABE.so -shared -fPIC -I /home/ayanb/Desktop/jdk1.6.0_18/include -I /home/ayanb/Desktop/jdk1.6.0_18/include/linux array.c item.c irt.c sort_index.c utilityFun.c genRandomDis.c getPar.c cat.c -I.
