550dc5a6df13f4fbc6e5418c1765971c4c343718
ncohen <>
2012-10-12 17:57:25 +0000

    *** empty log message ***

---
+++ TICalcDemo/src/com/ctb/demo/CalcDemo.java
@@ -0,0 +1,50 @@
+package com.ctb.demo;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import com.ti.eps.emu84.testAgency.EmulatorComponent;
+
+public class CalcDemo {
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("CalcDemo");
+        
+        EmulatorComponent emu = new EmulatorComponent(frame);
+        
+        emu.setFaceSize(EmulatorComponent.MEDIUM);
+        emu.ResetEmulator();
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel emptyLabel = new JLabel("");
+        //emptyLabel.setPreferredSize(new Dimension(175, 100));
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+
+        //Display the window.
+        frame.getContentPane().add(emu);
+        frame.pack();
+        
+        frame.setVisible(true);
+        
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
+
---
+++ TICalcDemo/start.bat
@@ -0,0 +1,1 @@
+java -Djava.library.path=./lib -cp ./bin;./lib/emu84TA.jar com.ctb.demo.CalcDemo
