e973aebc3296456aff6f7cd7d0ba0fbde5a44ca3
soumen <>
2012-10-31 13:10:02 +0000

    Change for OK Calculator

--- application-tdc/src/com/ctb/tdc/web/utils/CalculatorDialog.java
+++ application-tdc/src/com/ctb/tdc/web/utils/CalculatorDialog.java
@@ -1,22 +1,56 @@
 package com.ctb.tdc.web.utils;
 
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
+import javax.swing.AbstractButton;
+import javax.swing.Action;
 import javax.swing.JDialog;
+import javax.swing.JMenu;
+
 
 public class CalculatorDialog extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private boolean calculatorRunning;
 	
+	static {
+		  CalculatorDialog.setDefaultLookAndFeelDecorated(true);
+	}
+	
 	public CalculatorDialog(Frame frame, String title){
 		super(frame);
         this.setTitle(title);
         //in case the user closes the window
-        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        this.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
          //enables Window Events on this Component
-        this.addWindowListener(new CalculatorWindowLintener());
+        //this.addWindowListener(new CalculatorWindowLintener());
+        this.addComponentListener(new ComponentAdapter() {
+        	@Override
+        	public void componentMoved(ComponentEvent e) {
+        		CalculatorDialog dialog = (CalculatorDialog) e.getSource();
+        		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        		int maximumWidth = Double.valueOf(screenSize.getWidth()).intValue() - 300;
+        		int maximumHeight = Double.valueOf(screenSize.getHeight()).intValue() - 600;
+        		if (dialog.getLocation().x < 0) {
+                	dialog.setLocation(new Point(0, dialog.getLocation().y));
+                } else if (dialog.getLocation().y < 0) {
+                	dialog.setLocation(new Point(dialog.getLocation().x, 0));
+                } else if (dialog.getLocation().x > maximumWidth) {
+                	dialog.setLocation(new Point(maximumWidth, dialog.getLocation().y));
+                } else if (dialog.getLocation().y > maximumHeight) {
+                	dialog.setLocation(new Point(dialog.getLocation().x, maximumHeight));
+                }
+            }
+        });
         this.calculatorRunning = true;
+        removeCloseButton(this);
     }
 	
 	@Override
@@ -32,4 +66,24 @@ public class CalculatorDialog extends JDialog {
 	public void setCalculatorRunning(boolean calculatorRunning) {
 		this.calculatorRunning = calculatorRunning;
 	}
+	
+	public static void removeCloseButton(Component comp) {
+        if (comp instanceof JMenu) {
+          Component[] children = ((JMenu) comp).getMenuComponents();
+          for (int i = 0; i < children.length; ++i)
+            removeCloseButton(children[i]);
+        }
+        else if (comp instanceof AbstractButton) {
+          Action action = ((AbstractButton) comp).getAction();
+          String cmd = (action == null) ? "" : action.toString();
+          if (cmd.contains("CloseAction")) {
+            comp.getParent().remove(comp);
+          }
+        }
+        else if (comp instanceof Container) {
+          Component[] children = ((Container) comp).getComponents();
+          for (int i = 0; i < children.length; ++i)
+            removeCloseButton(children[i]);
+        }
+     }
 }
