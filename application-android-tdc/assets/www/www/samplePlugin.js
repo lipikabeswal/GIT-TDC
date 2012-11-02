c66256e827ecbb6697cbc8de031c77f6cc12c622
santhana <>
2012-11-02 15:25:34 +0000

    *** empty log message ***

---
+++ application-android-tdc/assets/www/www/samplePlugin.js
@@ -0,0 +1,15 @@
+var SamplePlugin = function()
+{
+};
+ 
+SamplePlugin.prototype.LOGIN = function(successCallback, failureCallback, jsonInput)
+{
+
+	return PhoneGap.exec(
+            successCallback,        //Function called upon success
+            failureCallback,        //Function called upon error
+            'DelegatePlugin',        //Tell PhoneGap to run "DelegatePlugin" Plugin
+            'LOGIN',                //Tell the which action we want to perform, matches an "action" string
+            [jsonInput]);           //A list of args passed to the plugin, in this case empty
+};
+ 
