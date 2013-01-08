772253e328bc254b6939025a9a7a2587cbc773bf
snandi <>
2013-01-08 12:52:27 +0000

    Code check in - Laslinks Code Upgrade

---
+++ application-tdc/webapp/includes/laslinks_manager.js
@@ -0,0 +1,40 @@
+var curLasItemId;
+var curLasItemHash;
+var curLasItemKey;
+var lasLinkState = {};
+function sendItemDetLasRenderer(item) {
+	item = item.split(",");
+	curLasItemId = item[0];	
+	curLasItemHash = item[1];
+	curLasItemKey = item[2];
+	alert(curLasItemId);
+}
+
+function getItemDetLasRenderer(){
+	return curLasItemId +"||"+ curLasItemHash+"||"+curLasItemKey;
+}
+
+function toggleSelState(sel){
+	var sel = msg.split("||");
+	var elemId = sel[1];
+	sel = sel[0];
+	console.log("toggleSelState", sel);
+	var elementFrm = document.getElementById(elemId);
+	//alert(elementFrm.length);
+	if(elementFrm != null ) {
+		elementFrm.contentWindow.toggleSelStateFrame(sel);
+	}
+
+}
+
+function getIsAnswered(id){
+	return lasLinkState[id];
+}
+
+function saveState(id,resp,state){
+ lasLinkState[id] = {resp: resp, state: state};
+}
+
+function checkItemPresence(id) {
+	return lasLinkState[id];
+}
