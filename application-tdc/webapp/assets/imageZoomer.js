a05def80c7181c2bdf6d7484441342d6dfea7ea9
mishra <>
2013-03-06 15:07:53 +0000

    Magnifier defect fixes - lipika

--- application-tdc/webapp/assets/imageZoomer.js
+++ application-tdc/webapp/assets/imageZoomer.js
@@ -42,16 +42,16 @@ var ddpowerzoomer={
 		var y = jQuery("#magnifierWindow").offset().top - coords.top ;
 		
 		if(y > (document.body.clientHeight)/2) {
-			y = y + 50;
+			y = y + 48;
 		}else if(y < (document.body.clientHeight)/2 && y > (document.body.clientHeight * .25)) {
 			y = y + 15;
 		}
 		
 		if(x < (document.body.clientWidth)/2) {
-			x = x + 50;
-		}/*else if(x > (document.body.clientWidth * .70)) {
-			x = x + 30;
-		}*/
+			x = x + 8;
+		}else if(x > (document.body.clientWidth)/2) {
+			x = x + 115;
+		}
 		
 		
 		
@@ -98,7 +98,7 @@ var ddpowerzoomer={
 		var $magnifier=$('<div style="position:absolute;z-index:1000000;width:'+parseInt(ddpowerzoomer.$zommersettings.magnifiersize[0])+ 'px;height:'+parseInt(ddpowerzoomer.$zommersettings.magnifiersize[1])+'px;display:none;" />')
 			.append('<div  id="magnifierWindow" style="width:'+ddpowerzoomer.$zommersettings.magnifiersize[0]+ 'px;height:'+ddpowerzoomer.$zommersettings.magnifiersize[1]+'px;overflow:hidden; border:8px solid #333333;"><div style="position:relative;left:0;top:0;" /></div>')
 			.appendTo(document.body).draggable({
-			containment: [0, 0,document.body.offsetWidth - 318,$("body").height()-120],	
+			containment: [0, 0,document.body.offsetWidth - 316,$("body").height()-116],	
 			start: function(event,ui) {
 			    //alert(window.isDragging);
 			    if(isDragging == 0){
