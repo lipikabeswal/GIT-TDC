295bc8dc1bc31135cf1606b57d05a351b581359e
nupurs <>
2013-06-20 14:13:06 +0000

    Changes forCEF LDB Linking in Open Suse - Shayan

--- application-las-tdcinstall/installer/artifacts/lockdownbrowser/linux/libcheck.sh
+++ application-las-tdcinstall/installer/artifacts/lockdownbrowser/linux/libcheck.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
-
 #look for library libnss3.so.1d
-lib1=$(locate libnss3.so.1d |  grep /usr/lib)
+lib1=$(find /usr/lib -name libnss3.so.1d)
 #check if string returned is not empty
 if [ -n "$lib1" ]; then
 	echo $lib1 "found"
 	#rm "$lib1"
 else
 #if string is empty, look for just the so file.
-	lib1=$(locate libnss3.so | grep /usr/lib/ | grep -v 'firefox\|thunderbird\|chrome\|chromium'| head -n 1)
+	lib1=$(find /usr/lib -name libnss3.so | grep -v 'firefox\|thunderbird\|chrome\|chromium'| head -n 1)
 	if [ -n "$lib1" ]; then
 		#link
 		ln -s "$lib1" ChromiumLDB/libnss3.so.1d
@@ -19,14 +17,14 @@ else
 	fi
 fi
 #look for library libnssutil3.so.1d
-lib1=$(locate libnssutil3.so.1d |  grep /usr/lib)
+lib1=$(find /usr/lib -name libnssutil3.so.1d)
 #check if string returned is not empty
 if [ -n "$lib1" ]; then
 	echo $lib1 "found"
 	#rm "$lib1"
 else
 #if string is empty, look for just the so file.
-	lib1=$(locate libnssutil3.so | grep /usr/lib/ | grep -v 'firefox\|thunderbird\|chrome\|chromium'| head -n 1)
+	lib1=$(find /usr/lib -name libnssutil3.so | grep -v 'firefox\|thunderbird\|chrome\|chromium'| head -n 1)
 	if [ -n "$lib1" ]; then
 		#link
 		ln -s "$lib1" ChromiumLDB/libnssutil3.so.1d
@@ -35,14 +33,14 @@ else
 		echo "libnssutil3.so.1d is not installed"
 	fi
 fi
-lib1=$(locate libsmime3.so.1d |  grep /usr/lib)
+lib1=$(find /usr/lib -name libsmime3.so.1d)
 #check if string returned is not empty
 if [ -n "$lib1" ]; then
 	echo $lib1 "found"
 	#rm "$lib1"
 else
 #if string is empty, look for just the so file.
-	lib1=$(locate libsmime3.so |  grep /usr/lib | grep -v 'firefox\|thunderbird\|chrome\|chromium'| head -n 1)
+	lib1=$(find /usr/lib -name libsmime3.so | grep -v 'firefox\|thunderbird\|chrome\|chromium'| head -n 1)
 	if [ -n "$lib1" ]; then
 		#link
 		ln -s "$lib1" ChromiumLDB/libsmime3.so.1d
@@ -51,14 +49,14 @@ else
 		echo "libsmime3.so.1d is not installed"
 	fi
 fi
-lib1=$(locate libplc4.so.0d |  grep /usr/lib)
+lib1=$(find /usr/lib -name libplc4.so.0d)
 #check if string returned is not empty
 if [ -n "$lib1" ]; then
 	echo $lib1 "found"
 	#rm "$lib1"
 else
 #if string is empty, look for just the so file.
-	lib1=$(locate libplc4.so | grep /usr/lib/ | grep -v 'firefox\|thunderbird\|chrome\|chromium'| head -n 1)
+	lib1=$(find /usr/lib -name libplc4.so | grep -v 'firefox\|thunderbird\|chrome\|chromium'| head -n 1)
 	if [ -n "$lib1" ]; then
 		#link
 		ln -s "$lib1" ChromiumLDB/libplc4.so.0d
@@ -67,20 +65,19 @@ else
 		echo "libnss3.so.1d is not installed"
 	fi
 fi
-lib1=$(locate libnspr4.so.0d |  grep /usr/lib)
+lib1=$(find /usr/lib -name libnspr4.so.0d)
 #check if string returned is not empty
 if [ -n "$lib1" ]; then
 	echo $lib1 "found"
 	#rm "$lib1"
 else
 #if string is empty, look for just the so file.
-	lib1=$(locate libnspr4.so | grep /usr/lib/ | grep -v 'firefox\|thunderbird\|chrome\|chromium'| head -n 1)
+	lib1=$(find /usr/lib -name libnspr4.so | grep -v 'firefox\|thunderbird\|chrome\|chromium'| head -n 1)
 	if [ -n "$lib1" ]; then
 		#link
-		ln -s "$lib1" ChromiumLDB/libplc4.so.0d
+		ln -s "$lib1" ChromiumLDB/libnspr4.so
 		echo $lib1 "link created"
 	else
 		echo "libnss3.so.1d is not installed"
 	fi
 fi
-
