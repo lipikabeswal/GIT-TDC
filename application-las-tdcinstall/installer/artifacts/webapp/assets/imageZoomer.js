405b6530d2dcabb5328011a7ce5e424e7cc72cae
mishra <>
2013-06-28 15:13:37 +0000

    Laslink Defect fixes

--- application-las-tdcinstall/installer/artifacts/webapp/assets/imageZoomer.js
+++ application-las-tdcinstall/installer/artifacts/webapp/assets/imageZoomer.js
@@ -122,9 +122,6 @@ var ddpowerzoomer={
 			},
 			start: function(event,ui) {
 			    //alert(window.isDragging);
-			    if(gScratchpad.visible){
-			    	gController.setAttribute('isMousedownOnScratchpad',true);
-			    }
 			    if(isDragging == 0){
 			    isDragging = 1;				
 			    jQuery("#magnifierWindow").css('visibility','hidden');
