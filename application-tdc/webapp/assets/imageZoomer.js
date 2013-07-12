2fc2d9456e2fb565af63f90125a454bca24cd8c7
lipikab <>
2013-07-12 05:57:49 +0000

    sticking issue reverted back - subhankar

--- application-tdc/webapp/assets/imageZoomer.js
+++ application-tdc/webapp/assets/imageZoomer.js
@@ -241,9 +241,7 @@ jQuery.fn.initMagnify=function(options){
 		}
 		
 		$("#magnifierWindow").on("mousedown",function() {
-			$('#scratchpad').blur();	
-			gMaskingRuler.dragger.remove();
-			gScratchpad.onmouseup.sendEvent();
+			$('#scratchpad').blur();
         });
 
 }
