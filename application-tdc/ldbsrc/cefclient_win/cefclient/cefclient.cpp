b2b5673def295b56f7e9d9e535b99572dd845ef6
shayanr <>
2014-06-18 06:47:05 +0000

    CEFLDB Cleaned Up

---
+++ application-tdc/ldbsrc/cefclient_win/cefclient/cefclient.cpp
@@ -0,0 +1,78 @@
+// Copyright (c) 2013 The Chromium Embedded Framework Authors. All rights
+// reserved. Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file.
+
+#include "cefclient/cefclient.h"
+#include <stdio.h>
+#include <cstdlib>
+#include <sstream>
+#include <string>
+#include "include/cef_app.h"
+#include "include/cef_browser.h"
+#include "include/cef_command_line.h"
+#include "include/cef_frame.h"
+#include "include/cef_runnable.h"
+#include "include/cef_web_plugin.h"
+#include "cefclient/client_handler.h"
+#include "cefclient/client_switches.h"
+#include "cefclient/string_util.h"
+#include "cefclient/util.h"
+
+CefRefPtr<ClientHandler> g_handler;
+CefRefPtr<CefCommandLine> g_command_line;
+
+CefRefPtr<CefBrowser> AppGetBrowser() {
+  if (!g_handler.get())
+    return NULL;
+  return g_handler->GetBrowser();
+}
+
+CefWindowHandle AppGetMainHwnd() {
+  if (!g_handler.get())
+    return NULL;
+  return g_handler->GetMainHwnd();
+}
+
+void AppInitCommandLine(int argc, const char* const* argv) {
+  g_command_line = CefCommandLine::CreateCommandLine();
+#if defined(OS_WIN)
+  g_command_line->InitFromString(::GetCommandLineW());
+#else
+  g_command_line->InitFromArgv(argc, argv);
+#endif
+}
+
+// Returns the application command line object.
+CefRefPtr<CefCommandLine> AppGetCommandLine() {
+  return g_command_line;
+}
+
+// Returns the application settings based on command line arguments.
+void AppGetSettings(CefSettings& settings) {
+  //ASSERT(g_command_line.get());
+  if (!g_command_line.get())
+    return;
+
+  CefString str;
+
+#if defined(OS_WIN)
+  settings.multi_threaded_message_loop =
+      g_command_line->HasSwitch(cefclient::kMultiThreadedMessageLoop);
+#endif
+
+  CefString(&settings.cache_path) =
+      g_command_line->GetSwitchValue(cefclient::kCachePath);
+
+  // Specify a port to enable DevTools if one isn't already specified.
+  if (!g_command_line->HasSwitch("remote-debugging-port"))
+    settings.remote_debugging_port = 8088;
+}
+
+bool AppIsOffScreenRenderingEnabled() {
+  //ASSERT(g_command_line.get());
+  if (!g_command_line.get())
+    return false;
+
+  return g_command_line->HasSwitch(cefclient::kOffScreenRenderingEnabled);
+}
+
