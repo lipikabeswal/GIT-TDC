ab2deca1f70f0e06b148ac114b6375f287712edf
poulamic <>
2014-07-09 10:24:03 +0000

    Fix problem with webserver not starting when opening multiple windows for application.
    The problem is caused by the fact that the cleanup of the HTTP server does not work when the application window closes. Upon launch of another window, the HTTP server launch failed since the port is not free.
    
    Solution:
    A counter in background.js counts how many windows have been opened since the browser has been launched. Upon each launch the counter is increased. In index.js the counter is added to the base port number of 10000, so the first window opens with the server running at  10001, then 10002, and so on.
    
    Changes description
    
    File: chromeos/background.js
    a) chrome.runtime.onInstalled handler added. In the handler the appInfo object is written into local storage. The object contains the counter for the port number to be used.
    b) getTimesLoaded() function added, which returns the counter value.
    
    File: chromeos/index.js
    a) tdc.http{} object with basePort property added.
    b) function launchWebServer(port) added.
    c) The code for loading the login.html into the webview and launching the OpenLaszlo app has been moved into the chrome.runtime.getBackgroundPage callback function inside the window.load event handler.
    
    File: chromeos/http.js
    a) Docstrings added.
    
    File: manifest.json
    a) storage permission added.
    
    File: README.TXT
    a) Documentation updated.
    
    
    File cleanup:
    File www/common.js removed; Code for NaCL removed from login.html.

--- tdc-chromebook/chromeos/background.js
+++ tdc-chromebook/chromeos/background.js
@@ -1,8 +1,38 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
+var appInfo = {
+  launches: 0
+}
+
+chrome.app.runtime.onLaunched.addListener( function() {
+  appInfo.launches++;
+  console.log("chrome.app.runtime.onLaunched: appInfo.launches=" + appInfo.launches);
+  chrome.app.window.create( 'index.html', {
     width: 800,
     height: 650,
     minWidth: 800,
     minHeight: 650
-  });
-});
+  } );
+} );
+
+chrome.app.runtime.onRestarted.addListener( function() {
+  console.log( "onRestarted" );
+} );
+
+chrome.runtime.onInstalled.addListener( function() {
+  console.log( "chrome.runtime.onInstalled" );
+  var storedInfo = chrome.storage.local.get( "appInfo", function( items ) {
+    if ( items.appInfo === undefined ) {
+      chrome.storage.local.set( {"appInfo": JSON.stringify( appInfo )} );
+    }
+    console.log( "storage get callback: items.appInfo=" + items.appInfo );
+
+  } );
+  console.log( "storedInfo=" + storedInfo );
+} );
+
+chrome.runtime.onSuspend.addListener( function() {
+  console.warn("onSuspend");
+})
+
+function getTimesLoaded() {
+  return appInfo.launches;
+}
--- tdc-chromebook/chromeos/index.js
+++ tdc-chromebook/chromeos/index.js
@@ -1,51 +1,85 @@
+/*
+ * Please refer to the README.TXT file in the parent folder for an overview
+ * of the application's files.
+ */
+
 // Global namespace for TDC
 var tdc = {};
 
-function $(id) {
-    return document.getElementById(id);
+function $( id ) {
+  return document.getElementById( id );
 }
 
-function log(text) {
-    $('log').value += text + '\n';
+function log( text ) {
+  $( 'log' ).value += text + '\n';
 }
 
-var port = 9999;
-var isServer = false;
+// The basePort is the port used when the first application window is
+// launched. On subsequent launches the port number will be increased by
+// 1, therefore the 2nd window will use port basePort + 1 for the HTTP
+// server. The values are stored inside
+tdc.http = {
+  basePort: 10000,  // The value of the getTimesLoaded() function call
+                    // in background.js will be added to the basePort value;
+  webviewHTMLFile: 'login.html'
+}
+
 var webviewMan = null;
-var webviewHTMLFile = 'login.html';
 
-if (http.Server) {
+
+// Code which needs to be executed for the DOMContentLoaded event of the
+// index.html goes here.
+document.addEventListener( 'DOMContentLoaded', function() {
+  console.log( "DOMContentLoaded event" );
+} );
+
+/**
+ * Lauches HTTP server with given port.
+ * @param port
+ */
+function launchWebServer( port ) {
+  if ( http.Server ) {
     // Listen for HTTP connections.
-    var server = new http.Server();
-    server.listen(port);
-    isServer = true;
+    var server = tdc.server = new http.Server();
+    server.listen( port );
+    server.addEventListener( 'request', function( req ) {
+      var url = req.headers.url;
+      if ( url == '/' )
+        url = '/www/' + tdc.http.webviewHTMLFile;
+      // Serve the pages of this chrome application.
+      req.serveUrl( url );
+      return true;
+    } );
 
-    server.addEventListener('request', function (req) {
-        var url = req.headers.url;
-        if (url == '/')
-            url = '/www/' + webviewHTMLFile;
-        // Serve the pages of this chrome application.
-        req.serveUrl(url);
-        return true;
-    });
+  }
 
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    console.log("DOMContentLoaded event");
-    /*webviewMan = new (WebviewManager);
-    webviewMan.initialize('tdcWebview');
-    webviewMan.loadPage('http://localhost:' + port + '/www/' + webviewHTMLFile);*/
-    //webviewMan.loadPage('https://oastest.ctb.com/SessionWeb/login.jsp');	//just to experiment
-});
-
-window.addEventListener("load", function (e) {
-
+/**
+ * When the window.load event is fired, the following steps
+ * have to executed:
+ *   a) Using the background.js getTimesLoaded() function
+ *      we know how many windows have been launched, and
+ *      which port has to be used when launching the HTTP
+ *      server.
+ *   b) The login.html has to be loaded into the webview tag.
+ *   c) ContentAction class is used start the login and content
+ *      loading.
+ */
+window.addEventListener( "load", function( e ) {
+  console.log( "window.load event" );
+  chrome.runtime.getBackgroundPage( function( w ) {
+    var timesLoaded = w.getTimesLoaded();
+    var port = tdc.http.basePort + timesLoaded;
+    console.log( "Application has been loaded %s times.", timesLoaded );
+    launchWebServer( port );
     webviewMan = new (WebviewManager);
-    webviewMan.initialize('tdcWebview');
-    webviewMan.loadPage('http://localhost:' + port + '/www/' + webviewHTMLFile);
+    webviewMan.initialize( 'tdcWebview' );
+    var url = 'http://localhost:' + port + "/www/" + tdc.http.webviewHTMLFile;
+    webviewMan.loadPage( url );
     zip.workerScriptsPath = "/chromeos/util/";
     zip.useWebWorkers = false;
     console.log("window.load event");
-    tdc.contentAction = new ContentAction();
-});
+    tdc.contentAction = new ContentAction();    
+  } );
+} );
