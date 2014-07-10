0926560aa7eef62308d72107ed15753631b08228
poulamic <>
2014-07-10 09:33:03 +0000

    Project cleanup Moved all files for the Chrome Packagd App into the subfolder "chrome-app". The Ant build_dhtml.xml file has been modified so that the TestClient.js file will be copied into the correct subfolder.

--- tdc-chromebook/chrome-app/chromeos/Persistence.js
+++ tdc-chromebook/chrome-app/chromeos/Persistence.js
@@ -8,7 +8,7 @@ PersistenceAction.loginResponse=null;
 PersistenceAction.prototype.login = function(reqXml)
 {
     console.log("Inside Login method");
-    var url = "http://tms-oas-qa.ctb.com/TestDeliveryWeb/CTB/login.do";
+    var url = "http://oasdelivery.ctb.com/TestDeliveryWeb/CTB/login.do";
     var posting = $.post(url, { method: "login", requestXML: reqXml});
 
     posting.done(function (data) {
@@ -27,7 +27,7 @@ PersistenceAction.prototype.login = function(reqXml)
 PersistenceAction.prototype.save = function(reqXml)
 {
     console.log("Inside save method");
-    var url = "http://tms-oas-qa.ctb.com/TestDeliveryWeb/CTB/save.do";
+    var url = "http://oasdelivery.ctb.com/TestDeliveryWeb/CTB/save.do";
     var posting = $.post(url, { method: "save", requestXML: reqXml});
 
     posting.done(function (data) {
